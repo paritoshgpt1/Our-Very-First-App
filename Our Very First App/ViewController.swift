12fa03513ad814b5c745f626f9cf96fd86819968
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2017-05-23 11:54:45 +0530

    completed the app

--- Our Very First App/ViewController.swift
+++ Our Very First App/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var image: UIView!
+    @IBOutlet weak var labelWalter: UILabel!
+    @IBOutlet weak var labelParitosh: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +26,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func change(_ sender: Any) {
+        labelWalter.text = "Button Clicked"
     }
 
 
